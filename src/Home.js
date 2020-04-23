c552f103b6e7c6cdbfb4aacd8886c9e0c3f78c0b
221249 <santhanakrishnan.g@cognizant.com>
2020-04-23 11:33:56 +0530

    regional data populated

--- src/Home.js
+++ src/Home.js
@@ -30,6 +30,28 @@ export default class Home extends Component{
                     </tr>
                 </tbody>
                 </Table>
+                <h2>covid regional stats</h2>
+                <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>State</th>    
+                    <th>Confirmed</th>
+                    <th>Discharged</th>
+                    <th>Casualty</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.covid19Data?.data.regional.map(indlState => (
+                        <tr>
+                        <td>{indlState.loc}</td>
+                        <td>{indlState.totalConfirmed}</td>
+                        <td>{indlState.discharged}</td>
+                        <td>{indlState.deaths}</td>
+                        </tr>
+                        ))}
+
+                </tbody>
+                </Table>
         </div>
         )
     }
